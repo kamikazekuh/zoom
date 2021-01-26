66ff6832340423f253937bef7f4baab773739bfb
kamikazekuh <33634496+kamikazekuh@users.noreply.github.com>
2021-01-26 18:56:24 +0100

    Update zoom.py

--- addons/source-python/plugins/zoom/zoom.py
+++ addons/source-python/plugins/zoom/zoom.py
@@ -1,71 +1,38 @@
-# =============================================================================
-# >> IMPORTS
-# =============================================================================
 from commands.client import ClientCommand
-from listeners import OnPlayerRunCommand
+from entities.hooks import EntityCondition
+from entities.hooks import EntityPreHook
+from listeners import get_button_combination_status
+from listeners import ButtonStatus
+from listeners import OnButtonStateChanged
+from memory import make_object
 from players.constants import PlayerButtons
 from players.entity import Player
+from weapons.entity import Weapon
 
-# =============================================================================
-# >> CONFIG
-# =============================================================================
 zoom_button = PlayerButtons.ATTACK2
 zoom_level = 30
 zoom_weapons = ['weapon_357']
-zoom_exclude = ['weapon_crossbow']
 
-# =============================================================================
-# >> CLASSES
-# =============================================================================
-class ZoomPlayer(Player):
-    def __init__(self, index, caching=True):
-        super().__init__(index, caching)
-        self.pressed = False
-        self.in_zoom = False
-        
-    def toggle_zoom(self):
-        weapon = self.active_weapon
-        if weapon is None or weapon.classname not in zoom_weapons:
+def toggle_zoom(player):
+    weapon = player.active_weapon
+    if weapon is None or weapon.classname not in zoom_weapons:
+        return
+
+    player.fov = player.default_fov if player.fov == zoom_level else zoom_level
+
+if zoom_button is not None:
+    @OnButtonStateChanged
+    def on_buttons_state_changed(player, old, new):
+        if get_button_combination_status(old, new, zoom_button) != ButtonStatus.PRESSED:
             return
 
-        self.fov = self.default_fov if self.fov == zoom_level else zoom_level 
+        toggle_zoom(player)
 
-# =============================================================================
-# >> COMMANDS
-# =============================================================================
 @ClientCommand('do_zoom')
-def _zoom_client_command(command,index,team_only=False):
-    player = ZoomPlayer(index)
-    weapon = player.active_weapon
-    if weapon is None: return
-    if weapon.classname in zoom_weapons:
-        player.toggle_zoom()        
+def do_zoom(command, index, team_only=False):
+    toggle_zoom(Player(index))
 
-# =============================================================================
-# >> LISTENERS
-# =============================================================================
-if zoom_button != None:        
-    @OnPlayerRunCommand
-    def on_player_run_command(ply, user_cmd):
-        player = ZoomPlayer(ply.index)
-        weapon = player.active_weapon
-        default_pov = player.default_fov
-        if weapon is None: return
-        if user_cmd.buttons & zoom_button:
-            if weapon.classname in zoom_weapons:
-                if not player.pressed:
-                    if not player.in_zoom:
-                        player.pressed = True
-                        player.fov = zoom_level
-                        player.in_zoom = True
-                    else:
-                        player.fov = default_pov
-                        player.in_zoom = False
-                        player.pressed = True
-        else:
-            if player.pressed == True:
-                player.pressed = False
-        if weapon.classname not in zoom_weapons and weapon.classname not in zoom_exclude:
-            player.fov = default_pov
-            player.in_zoom = False
-            player.pressed = False
+@EntityPreHook(EntityCondition.is_player, 'weapon_switch')
+def pre_weapon_switch(stack_data):
+    player = make_object(Player, stack_data[0])
+    player.fov = player.default_fov
